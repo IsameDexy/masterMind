335458d00098ef3cab59fdf28520eefd27bcdace
IlonkaPC\Sam <isamedexy@gmail.com>
2019-12-15 20:24:18 +0100

    #003 Create color array

---
+++ src/components/ColorButton.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+/**
+ * Color Array will keep the colors which can be picked. !!!!!Needs to be in its own file
+ */
+const colorArray = ['#ff0000', '#00ff00', '#0000ff', '#fff000', '#000fff', '#ffa500']
+
+class ColorButton extends Component {
+  render() {
+    return (
+      <div>Hier komt de colorbutton</div>
+    );
+  }
+}
+
+export default ColorButton;
